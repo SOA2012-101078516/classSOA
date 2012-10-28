0523431efa9c8a8b9458c696b7cca3fbec822fda
Ubuntu <ubuntu@ip-10-170-91-23.us-west-1.compute.internal>
2012-10-28 16:37:17 +0000

    create my potato to 4-6

---
+++ myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '814f20a60e2684dfc2f82886f2cc87fbb953ca22159594f9dcc0ba295b3f5f8a165d81f753b6c03a7a2f4ab3ba37256ab10970dc3fc236420a963ef3be2e7d9a'
